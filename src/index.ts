ae9b5c70e4c7894435d2461540c9d6f2a880a9a6
jim3 <jimmy3.xyz@gmail.com>
2023-11-18 20:09:57 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
-// WiFi Analyzer written in TypeScript
-
-import axios from 'axios';
-import * as dotenv from 'dotenv';
-import * as util from 'util';
-import { Sender } from '@questdb/nodejs-client';
+import axios from "axios";
+import * as dotenv from "dotenv";
+import * as util from "util";
+import mqtt from "mqtt";
 
 dotenv.config();
-
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 const server = process.env.SERVER;
 const port = process.env.PORT;
 const url = `http://${username}:${password}@${server}:${port}`;
-const check_session = "/session/check_session";
 const accesspoints = "/devices/views/phydot11_accesspoints/devices.json";
 const rfsensor = "/devices/views/phy-RFSENSOR/devices.json";
+
+// mqtt option object
+const options = {
+    host: process.env.MQTT_SERVER as string,
+    port: process.env.MQTT_PORT as unknown as number,
+};
+const client = mqtt.connect(options); // connect to the mqtt broker
 
 // Interface for SensorData objects
 interface SensorData {
@@ -29,25 +32,13 @@ interface Clients {
     [ssid: string]: (string | number)[];
 }
 
-// ------------------------------------------------------- //
-
-const checkSession = async () => {
-    try {
-        const response = await axios.get(`${url}${check_session}`);
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
-};
-checkSession();
-
 // -------------------------------------------------------- //
 
 const getRFSensors = async (): Promise<SensorData[]> => {
     const response = await axios.get(`${url}${rfsensor}`);
     const data = response.data;
+    let arr: SensorData[] = [];
 
-    let arr: SensorData[] = []; 
     try {
         for (let d of data) {
             if (d !== null) {
@@ -60,7 +51,8 @@ const getRFSensors = async (): Promise<SensorData[]> => {
         }
     } catch (error) {
         console.error("Error fetching RF sensors:", error);
-    }    
+    }
+    console.log(util.inspect(arr, false, null));
     return arr;
 };
 
@@ -68,9 +60,8 @@ const getRFSensors = async (): Promise<SensorData[]> => {
 
 const getRelatedClients = async (): Promise<Clients> => {
     const response = await axios.get(`${url}${accesspoints}`);
-    let data = response.data; 
+    let data = response.data;
     const clients = {};
-    // loop
     try {
         for (const d of data) {
             if (d !== null) {
@@ -80,66 +71,52 @@ const getRelatedClients = async (): Promise<Clients> => {
                     const response = await axios.get(
                         `${url}/phy/phy80211/related-to/${deviceKey}/devices.json` // get related clients
                     );
-                    const relatedClients = response.data; // (*)
-                    clients[ssid] = relatedClients; 
+                    const relatedClients = response.data;
+                    clients[ssid] = relatedClients;
                 }
             }
         }
     } catch (error) {
         console.error("Error fetching access points:", error);
     }
-    
-    return clients;
+    console.log(util.inspect(clients, false, null));
+    return clients; // { ssid: [ {}, {}, {} ] }
 };
 
 // -------------------------------------------------------- //
 
-async function run() {
-    // create an instance of `Sender` and connect to questDB server
-    const sender = new Sender();
-    await sender.connect({ port: 9009, host: "localhost" });
-
-    // rf sensors
-    const rfSensors = await getRFSensors(); // [{}, {}, {}}]
-    for (const sensor of rfSensors) {
-        const manufacturer = sensor.manufacturer;
-        const macaddr = sensor.macaddr;
-        const frequency = Number(sensor.frequency);
-        const last_time = Number(sensor.last_time);
-
-        sender
-            .table("sensors")
-            .stringColumn("manufacturer", manufacturer)
-            .stringColumn("macaddr", macaddr)
-            .floatColumn("frequency", frequency)
-            .timestampColumn("last_time", last_time)
-            .atNow();
+async function main() {
+    const rfSensors = await getRFSensors();
+    for (const s of rfSensors) {
+        const manufacturer = s.manufacturer;
+        const macaddr = s.macaddr;
+        const frequency = Number(s.frequency);
+        const last_time = Number(s.last_time);
+        client.publish(
+            "rf-sensors",
+            JSON.stringify({ manufacturer, macaddr, frequency, last_time })
+        );
     }
 
-    // clients
-    const results = await getRelatedClients(); // { ssid: [ {}, {}, {} ] }
+    // get clients
+    const results = await getRelatedClients();
     for (const r in results) {
         if (results.hasOwnProperty(r)) {
             const clients = results[r];
-
             clients.forEach((c) => {
                 const macaddr = c["kismet.device.base.macaddr"] || "";
                 const deviceName = c["kismet.device.base.manuf"] || "";
                 const last_time = c["kismet.device.base.last_time"] || "";
-
-                sender
-                    .table("clients")
-                    .stringColumn("ssid", r)
-                    .stringColumn("macaddr", macaddr)
-                    .stringColumn("deviceName", deviceName)
-                    .timestampColumn("last_time", last_time)
-                    .atNow();
+                // publish to mqtt
+                client.publish(
+                    "clients",
+                    JSON.stringify({ macaddr, deviceName, last_time })
+                );
             });
         }
     }
-
-    await sender.flush();
-    await sender.close();
-    return new Promise((resolve) => resolve(0));
+    // close mqtt connection
+    client.end();
 }
-run();
+
+main();
